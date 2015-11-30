bc563bcc8f1a587b66735ea3abdd34add8fa58a9
Christoph DeMaskey <christoph.demaskey@gmail.com>
2015-11-30 00:37:11 -0500

    update the raspberry sharp libraries to the .net 4.6 nuget package.

--- coffee-roaster/coffee-roaster/Program.cs
+++ coffee-roaster/coffee-roaster/Program.cs
@@ -1,18 +1,17 @@
 ﻿using Raspberry.IO.GeneralPurpose;
-using System;
 using System.Threading;
 
 namespace coffee_roaster
 {
-    internal class Program
+    public class Program
     {
-        private static void Main(string[] args)
+        public static void Main(string[] args)
         {
             var led1 = ConnectorPin.P1Pin07.Output();
 
             var connection = new GpioConnection(led1);
 
-            for(int i = 0; i < 100; i++)
+            for (int i = 0; i < 100; i++)
             {
                 connection.Toggle(led1);
                 Thread.Sleep(250);
