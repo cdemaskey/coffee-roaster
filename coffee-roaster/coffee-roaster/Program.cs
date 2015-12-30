e881d443e0ff1a31a876e4acd24378777303a456
Christoph DeMaskey <christoph.demaskey@gmail.com>
2015-12-29 20:58:37 -0500

    remove raspberry.io packages. add therm.c to the solution for reference.

--- coffee-roaster/coffee-roaster/Program.cs
+++ coffee-roaster/coffee-roaster/Program.cs
@@ -1,23 +1,10 @@
-﻿using Raspberry.IO.GeneralPurpose;
-using System.Threading;
-
-namespace coffee_roaster
+﻿namespace coffee_roaster
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            var led1 = ConnectorPin.P1Pin07.Output();
-
-            var connection = new GpioConnection(led1);
-
-            for (int i = 0; i < 100; i++)
-            {
-                connection.Toggle(led1);
-                Thread.Sleep(250);
-            }
-
-            connection.Close();
+            
         }
     }
 }
