c4e62c5ddf3f8a60737cb3e7c5f423bb51d1c655
Christoph DeMaskey <christoph.demaskey@gmail.com>
2016-01-10 15:01:31 -0500

    add a unit test project for the coffee-roaster project.

---
+++ tests/coffee-roaster.UnitTests/Ti430BoostAds1118ConnectionUnitTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace coffee_roaster.UnitTests
+{
+    [TestClass]
+    public class Ti430BoostAds1118ConnectionUnitTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
